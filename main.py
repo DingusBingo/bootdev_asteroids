2563070c9d6d7ef3ccd72bb32b965da01f7b458b
DingusBingo <christhehurley@gmail.com>
2025-03-23 14:32:32 -0400

    added delta, fps, circleshape class

--- main.py
+++ main.py
@@ -17,8 +17,13 @@ def draw_loop():
             return
     screen.fill(color_black)
     pygame.display.flip()
+    dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         draw_loop()
