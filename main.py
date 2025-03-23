9449466d4c67eed64b7218160e847827d69ceed5
DingusBingo <christhehurley@gmail.com>
2025-03-23 15:44:46 -0400

    added player rotation, fixed main game loop

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import pygame
 from constants import *
-from circleshape import *
-from player import *
+from player import Player
 
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 color_black = (0, 0, 0)
 
 def main():
     pygame.init()
-    
-
-
-def draw_loop():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    screen.fill(color_black)
-    player.draw(screen)
-    pygame.display.flip()
-    dt = game_clock.tick(60) / 1000
-
-if __name__ == "__main__":
-    main()
-    player = Player(SCREEN_HEIGHT/2, SCREEN_HEIGHT/2)
-
-    game_clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
-        draw_loop()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+        player.update(dt)
+
+        screen.fill(color_black)
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
--- player.py
+++ player.py
@@ -1,7 +1,7 @@
-from circleshape import *
-from constants import *
-from main import *
 import pygame
+from circleshape import CircleShape
+from constants import *
+
 
 color_white = (255, 255, 255)
 
@@ -20,3 +20,14 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, color_white, self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
