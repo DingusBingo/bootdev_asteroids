6481cfca245d2e1f1feb67b15f594513ac555996
DingusBingo <christhehurley@gmail.com>
2025-03-23 22:16:07 -0400

    added astroids

---
+++ asteroid.py
@@ -0,0 +1,15 @@
+import pygame
+from circleshape import CircleShape
+
+color_yellow = (255, 255, 0)
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, color_yellow, self.position, self.radius, 2)
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 color_black = (0, 0, 0)
@@ -12,8 +13,12 @@ def main():
     clock = pygame.time.Clock()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
     dt = 0
 
 
